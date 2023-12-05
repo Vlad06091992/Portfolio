cfbbc15b40ecdbaea6ba024f255e4f06e093fb59
Vlad <Smirnov.ru92@mail.ru>
2023-12-05 12:35:49 +0500

    add projects in portfolio

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,20 +1,47 @@
 import styles from './Project.module.scss'
 import React from 'react';
+import {Button} from "../ui/button/Button";
 
 
 export const Project = (props) => {
 
-    const src = props.image
-
     return (
         <div className={styles.project}>
-            <a target={'_blank'} href={props.link}>
-                <button className={styles.button}>view</button>
-            </a>
-            {/*<img className={css.image} src={`..${src}`}></img>*/}
-            <img className={styles.image} src={src}></img>
             <h3>{props.title}</h3>
+            <div className={styles.linkButtons}>
+            <div>
+                <a target={'_blank'} href={props.linkToVercel}>
+                    <Button description={'Link to vercel'}></Button>
+                </a>
+            </div>
+            <div>
+                <a target={'_blank'} href={props.linkFrontend}>
+                    <Button description={'Link to Github'}></Button>
+                </a>
+            </div>
+                {props.linkBackend && <div>
+                    <a target={'_blank'} href={props.linkBackend}>
+                        <Button description={'Link to Github(backend)'}></Button>
+                    </a>
+                </div>}
+            </div>
+            <div className={styles.main}>
+                <div>
+                    <img alt={'image'} className={styles.image} src={props.image}></img>
+                </div>
+                <div>
+                    <h3 className={styles.h3}>Библиотеки и инструменты</h3>
+                    <ul>
+                        {
+                            props.technologies.sort((a, b) => a.localeCompare(b)).map(el => <li
+                                className={styles.itemLi}>{el}</li>)
+                        }
+                    </ul>
+                </div>
+            </div>
+
             <div className={styles.description}>{props.description}</div>
+            {props.figma &&    <a target={"_blank"} href={props.figma}>Ссылка на figma</a>}
         </div>
 
     )
@@ -29,7 +56,7 @@ export const Project = (props) => {
 // export const Project = (props) => {
 //     return (
 //         <div className={css.job}>
-//             <div style={props.style } className={css.logo}><button>view</button></div>
+//             <div style={props.style } className={css.logo}><Button.jsx>view</Button.jsx></div>
 //             <h3>{props.title}</h3>
 //             <div className={css.description}>{props.description}</div>
 //         </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,44 +2,81 @@ import css from './Projects.module.scss'
 import React from 'react';
 import {Project} from '../Project/Project'
 import {Title} from "../../common/components/Title/Title";
-import petProjects from '../../assets/projects/pet_projects.png'
-import counter from '../../assets/projects/counter.png'
-import socialNetwork from '../../assets/projects/social_network.jpg'
-import todolist from '../../assets/projects/todolist.webp'
+import todolist from '../../assets/projects/todolist.png'
+import todolistTest from '../../assets/projects/todolist-test.png'
+import onlineShop from '../../assets/projects/online-shop.png'
+import gitHunt from '../../assets/projects/git-hunt.png'
 import Slide from "react-reveal/Slide";
 
 
-let projectsProps = [{
-    title: "Social network",
-    image: socialNetwork,
-    link: 'https://github.com/Vlad06091992/SocialNetwork-react-typescript'
-}, {
-    title: "Todolist",
-    image: todolist,
-    link: 'https://github.com/Vlad06091992/ToDoLIst-react-typescript'
+let projectsProps = [
+    {
+        title: "Online shop",
+        image: onlineShop,
+        linkFrontend: 'https://github.com/Vlad06091992/online-shop-test-client',
+        linkBackend: 'https://github.com/Vlad06091992/online-shop-test-server',
+        linkToVercel: 'https://online-shop-test-client.vercel.app/',
+        description: `Тестовое задание на позицию fullstack разработчика` ,
+        figma:"https://www.figma.com/file/oWzPN2t2g0lmUG1jyTdYxL/%D0%A2%D0%B5%D1%81%D1%82%D0%BE%D0%B2%D0%BE%D0%B5-%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5-%2F-Fullstack-Web-Developer?node-id=0%3A1&mode=dev",
+        technologies: ["Localstorage","Typescript","React", "Mobx","Tailwind CSS","Axios","React router dom V6","Express(back)","Vite","SCSS","Cors(Back)"],
 
-}, {
-    title: "Custom counter",
-    image: counter,
-    https:'https://github.com/Vlad06091992/react-counter'
-}, {
-    title: "Pet projects native JS",
-    image: petProjects,
-    link:'https://github.com/Vlad06091992/pet-projects-native-js'
-},]
+    },
+    {
+        title: "Tasks manager",
+        image: todolist,
+        linkFrontend: 'https://github.com/Vlad06091992/ToDoLIst-react-typescript',
+        linkToVercel: 'https://todo-cyan-alpha.vercel.app',
+        description: "Проект для планирования задач, взаимодействует с внешним API",
+        technologies: ["Typescript","React", "Redux Tool Kit","Axios", "Formik","React router dom V6","Storybook","Material ui"],
+
+    },
+    {
+        title: "Git Hunt",
+        image: gitHunt,
+        linkFrontend: 'https://github.com/Vlad06091992/GitHunt',
+        linkToVercel: 'https://git-hunt-phi.vercel.app',
+        description: "Проект для поиска пользователей на Github",
+        technologies: ["Typescript","Vue 3 composition API","Pinia","Vite","Axios","Element-plus","SCSS"],
+
+    },
+    {
+        title: "Infinite Todolist",
+        image: todolistTest,
+        linkFrontend: 'https://github.com/Vlad06091992/test-assigment',
+        linkToVercel: 'https://test-assigment-sand.vercel.app/',
+        description: "Тестовое задание на позицию frontend разработчика. Представляет собой тудулист с бесконочной вложенностью подзадач для каждой задачи",
+        technologies: ["Typescript","React","Mobx","Prettier","Localstorage","Material UI","SCSS"],
+        figma: "https://www.figma.com/file/9ptzO19xMujbgo0YVG6KTB/%D1%82%D0%B5%D1%81%D1%82?type=design&node-id=0-1&t=SQIDI8pmIiJsGxfe-0"
+
+    },
+
+    // {
+    //     title: "Social network",
+    //     image: socialNetwork,
+    //     link: 'https://github.com/Vlad06091992/SocialNetwork-react-typescript',
+    //     linkToVercel: 'https://github.com/Vlad06091992/SocialNetwork-react-typescript',
+    //     description: "",
+    //     technologies: "",
+    // },
+    // {
+    //     title: "Custom counter",
+    //     image: counter,
+    //     https: 'https://github.com/Vlad06091992/react-counter'
+    // },
+    // {
+    //     title: "Pet projects native JS",
+    //     image: petProjects,
+    //     link: 'https://github.com/Vlad06091992/pet-projects-native-js'
+    // },
+]
 
 export const Projects = () => {
     return (<div id={'projects'} className={css.projects}>
             <Title title={"My projects"}/>
             <Slide left>
-            <div className={css.container}>
-                {projectsProps.map((el, index) => {
-                    return (<Project key={index} image={el.image} title={el.title} description={el.decription}
-                                     link={el.link}/>
-
-                    )
-                })}
-            </div>
+                <div className={css.container}>
+                    {projectsProps.map((el, index) => <Project key={el.title} {...el}/>)}
+                </div>
 
             </Slide>
         </div>
