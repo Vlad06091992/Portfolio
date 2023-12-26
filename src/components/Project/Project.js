0ec8581a107cf0f60448411fa3babb277924b362
Vlad <Smirnov.ru92@mail.ru>
2023-12-27 00:30:11 +0500

    feat: swagger for test task(online shop)

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -48,6 +48,7 @@ export const Project = (props) => {
             <div className={styles.description}>{props.description}</div>
             {props.figma && <div><a target={"_blank"} href={props.figma} rel="noreferrer">Ссылка на figma</a></div>}
             {props.swagger && <div><a target={"_blank"} href={props.swagger} rel="noreferrer">Ссылка на swagger</a></div>}
+            {props.mySwaggerLink && <div><a target={"_blank"} href={props.mySwaggerLink} rel="noreferrer">Ссылка на swagger(реализованный мною backend)</a></div>}
             {props.linkToApi && <div><a target={"_blank"} href={props.linkToApi} rel="noreferrer">Ссылка на API(backend)</a></div>}
             {props.linkToTask && <div><a target={"_blank"} href={props.linkToTask} rel="noreferrer">Ссылка на задание(Github)</a></div>}
         </div>
